14e911acf31f9ce4c84afd5ce0cf5107c9c5a957
XYUU <xyuu@xyuu.net>
2018-11-05 12:01:17 +0800

    还原调试代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+import { Router } from '@reach/router';
+import App from './admin/default/App';
+import { Login } from './snippets/pages/Login';
 import { Lottery } from './snippets/pages/Lottery';
 
 const root = document.getElementById('root');
+const store = configureStore();
 
 ReactDOM.render(
-<Lottery default path="lottery" />
+    <Provider store={store}>
+        <Router>
+            <App default />
+            <Login path="login" />
+            <Lottery path="lottery" />
+        </Router>
+    </Provider>
     , root);
